87ae1d30f2a33a025123896875300fe4a11fc4f5
HJ <KGA@DESKTOP-78ORSNK>
2025-09-02 14:58:25 +0900

    오후 수업

--- Hpro/Hpro.cpp
+++ Hpro/Hpro.cpp
@@ -22,7 +22,37 @@ CTRL +B+B 프로젝트 빌드
 리빌드 : 전체를 다시 빌드하기 (정리 + 빌드)
 CTRL + B + R : 재빌드
 ctrl + 화살표 : 페이지 업 다운
+ctrl + k + c : 블럭 영역 전부 주석처리
+ctrl + k + u : 블럭 영역 전부 주석처리 해제
 */ 
+
+/*
+변수(Variable)
+ - 변하는 숫자
+ - 컴퓨터에 값을 기억시키기 위해 만들고 사용
+ - 메모리라는 곳에 저장
+
+ 연산자 (operator)
+- 계산을 하기 위한 기호
+- 대입 연산자
+  1. = 오른쪽에 있는 값을 왼쪽에 넣어라
+      @ a=b b에 있는 값을 a에 넣어라 
+- 산술 연산자
+  사칙연산 + - * /
+  % (나머지 연산)
+   a = 5%3 a는 2가 들어간다 ( 5를 3으로 나누었을 때 나머지가 2)
+- 복합 대입 연산자
+   줄여쓰기용
+   a+=b; //아래와 같은 코드
+   a = a+b; 
+- 증감 연산자
+    a++; //a에다가 1을 더해라
+    a--; //a에다가 1을 빼라
+
+*/
+
+
+
 
 #include <iostream> //입출력 관련
 #include <cstdio>   // stdio.h
@@ -37,18 +67,22 @@ int main() //앤트리 포인트 코드가 시작되는 곳
     printf("Hello World\n");
     printf("\"heejae\"\t");
     printf("98년생입니다.");
-    
-    int number = 0;
-    
-    scanf("%d", &number); //숫자를 하나 입력 받고(c)
-    printf("입력한 숫자는 : %d\n", number);
-   
-    std::cin >> number; //숫자를 하나 입력 받기(c++)
-    printf("입력한 숫자는 : %d\n", number);
-    
-    //프로그램을 실행 했을 때 나이를 물어보고 입력받은 숫자를 그대로 출력 아래
-
-    printf("나이를 입력하세요 : %d\n", number);
+//    
+//    //int : 정수형(소수점이 없는 숫자 ex/ 5,-1 ,0 ,66 ...)
+//    //number = 5.3;  // 소주점 아래는 모두 버림 5만 저장
+//    //number = 5 / 2; // 2.5 ===> 0.5 버림
+//
+//    int number;      // number라는 이름을 가진 int(인티저)변수를 선언하고 거기에 0을 대입한다.
+//     number = 0;   //변수에 값을 대입
+//    scanf("%d", &number); //숫자를 하나 입력 받고(c)
+//    printf("입력한 숫자는 : %d\n", number);
+//   
+//    std::cin >> number; //숫자를 하나 입력 받기(c++)
+//    printf("입력한 숫자는 : %d\n", number);
+//    
+//    //프로그램을 실행 했을 때 나이를 물어보고 입력받은 숫자를 그대로 출력 아래
+//
+   printf("나이를 입력하세요 : %d\n", number);
     return 0;
 
     // 이스케이프 스퀸스
@@ -64,7 +98,17 @@ int main() //앤트리 포인트 코드가 시작되는 곳
 //커밋 연습 ㅁㄴㅇㄻㄴㅇㄻㄴㅇㄹ
 // 추가할 내용
 
+int number1 = 0;
+int number2 = 0;
+printf("숫자 1을 입력하시오 : ");
+std::cin >> number1;
+printf("숫자 2를 입력하시오 : ");
+std::cin >> number1;
+int number3 = number1 + number2;
+printf("두 숫자의 합은 %d입니다.", number3);
 
+return 0;
+}
 
 
 // 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
